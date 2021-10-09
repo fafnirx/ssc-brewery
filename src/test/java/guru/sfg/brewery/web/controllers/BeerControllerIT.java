2d9eaf50b2876fb90677f4b8efd3beaf63450f8d
Mirko Leschikar <fafnirx@gmail.com>
2021-10-09 14:54:17 +0200

    First simple InMemoryUserDetailManager config

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -37,7 +37,7 @@ class BeerControllerIT extends BaseIT {
     @Test
     void findBeersWithHttpBasic() throws Exception {
         mockMvc.perform(get("/beers/find")
-                        .with(httpBasic("mirko", "verifyme")))
+                        .with(httpBasic("user", "password")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
